b2efbb2b2ea22d275bb1e6df19dfb4a5faca38e5
wildlifehexagon <crossthebreeze@gmail.com>
2018-05-19 18:51:06 -0500

    persist state to local storage and remove redux-logger

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import App from "./App"
-import configureStore from "store/store"
+import store from "store/store"
 import registerServiceWorker from "./registerServiceWorker"
 import "styles"
-
-const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,19 @@
 import { applyMiddleware, createStore } from "redux"
-import rootReducer from "../reducers"
-import logger from "redux-logger"
 import thunk from "redux-thunk"
+import rootReducer from "reducers"
+import { loadState, saveState } from "utils/localStorage"
 
-const configureStore = initialState => {
-  const store = createStore(
-    rootReducer,
-    initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(logger, thunk)
-  )
-  return store
-}
+const persistedState = loadState()
 
-export default configureStore
+const store = createStore(
+  rootReducer,
+  persistedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+)
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
+
+export default store
